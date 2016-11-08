585be1df647ece65529f16cc346fc67f49b220ff
Anurag_Dwivedi02 <Anurag_Dwivedi02@PUNITP106513L.ad.infosys.com>
2016-11-08 17:57:13 +0530

    First WebService

---
+++ WebService_SpringBoot/src/main/java/com/example/WebServiceSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootApplication.class, args);
+	}
+}
